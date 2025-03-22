9d0c876ffbee1b991e04b0b1be502e127ee7566c
Goodluck-Nnadi <goodlucknnadi050@gmail.com>
2025-03-22 14:49:46 +0100

    Initial commit: BlogPage React project

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,38 +2,63 @@ import React from "react";
 import { NavLink } from "react-router";
 
 function Pagination({ currentPage, totalPages, next, prev, gotoPage }) {
+  const generatePageNumbers = () => {
+    const pages = [];
+
+    if (totalPages <= 3) {
+      for (let i = 1; i <= totalPages; i++) {
+        pages.push(i);
+      }
+    } else {
+      if (currentPage <= 2) {
+        pages.push(1, 2, 3, "...");
+      } else if (currentPage >= totalPages - 1) {
+        pages.push("...", totalPages - 2, totalPages - 1, totalPages);
+      } else {
+        pages.push("...", currentPage - 1, currentPage, currentPage + 1, "...");
+      }
+    }
+
+    return pages;
+  };
+
+  const pages = generatePageNumbers();
+
   return (
     <nav>
       <ul className="pagination">
+        {/* Previous Button */}
         <li className="page-item">
-          <NavLink
-            className="page-link"
-            to="#"
-            onClick={() => prev()}
-          >
+          <NavLink className="page-link" to="#" onClick={() => prev()}>
             Previous
           </NavLink>
         </li>
-        {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
-          <li
-            key={page}
-            className={`page-item ${currentPage === page ? "active" : ""}`}
-          >
-            <NavLink
-              className="page-link"
-              to="#"
-              onClick={() => gotoPage(page)}
+
+        {/* Page Numbers */}
+        {pages.map((page, idx) =>
+          page === "..." ? (
+            <li key={idx} className="page-item disabled">
+              <span className="page-link">...</span>
+            </li>
+          ) : (
+            <li
+              key={page}
+              className={`page-item ${currentPage === page ? "active" : ""}`}
             >
-              {page}
-            </NavLink>
-          </li>
-        ))}
+              <NavLink
+                className="page-link"
+                to="#"
+                onClick={() => gotoPage(page)}
+              >
+                {page}
+              </NavLink>
+            </li>
+          )
+        )}
+
+        {/* Next Button */}
         <li className="page-item">
-          <NavLink
-            className="page-link"
-            to="#"
-            onClick={() => next()}
-          >
+          <NavLink className="page-link" to="#" onClick={() => next()}>
             Next
           </NavLink>
         </li>
